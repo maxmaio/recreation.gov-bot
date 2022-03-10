a778c92daed8739c6375b306fe4a14007845e7c9
maxmaio <maxmaio8@gmail.com>
2022-03-10 09:57:18 -0800

    remove close date

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import FirefoxProfile
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
 profile = FirefoxProfile("/Users/maxmaio/Library/Application Support/Firefox/Profiles/48s8diof.default-release")
 profile.update_preferences()
@@ -10,14 +11,12 @@ driver.get("https://www.recreation.gov/camping/campgrounds/234513")
 closeModal = driver.find_element(By.CLASS_NAME, "sarsa-modal-close-button")
 driver.execute_script("arguments[0].click();", closeModal)
 
+checkoutTime = time.time()
 startDateInput = driver.find_element(By.ID, "campground-start-date-calendar")
 startDateInput.send_keys("04/14/22" + Keys.ENTER)
 
 endDateInput = driver.find_element(By.ID, "campground-end-date-calendar")
 endDateInput.send_keys("04/22/22" + Keys.ENTER)
-
-# closeDate = driver.find_element(By.CLASS_NAME, "sarsa-heading-title")
-# driver.execute_script("arguments[0].click();", closeDate)
 
 startButton = driver.find_element(By.ID,"60171")
 childeren = startButton.find_elements(By.CLASS_NAME, "available")
@@ -28,3 +27,5 @@ driver.execute_script("arguments[0].click();", secondClick)
 
 addToCart = driver.find_element(By.CLASS_NAME, "availability-page-book-now-button-tracker")
 driver.execute_script("arguments[0].click();", addToCart)
+
+print(time.time()- checkoutTime)
